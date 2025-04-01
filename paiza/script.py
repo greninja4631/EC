2155ec5251d9b239c5fc318cd74cb9c2eb1399f1
joaoonvnjo <greninja4631@gmail.com>
2025-04-01 17:55:54 +0900

    pythonフォルダ作成しました

--- paiza/script.py
+++ paiza/script.py
@@ -1,4 +1,13 @@
-for i in range(1, 6):
-    for j in range(i):
-        print("*" , '')
-    print()
+numbers = [3, 7, 2, 8, 5]
+
+# リストの最初の要素を「仮の最大値」とする
+max_value = numbers[0]
+
+# リストの2番目以降の要素と「仮の最大値」を比較する
+for number in numbers[1:]:
+  if number > max_value:
+    # リストの要素の方が大きければ、「仮の最大値」を更新する
+    max_value = number
+
+# 最大値を出力する
+print(max_value)
