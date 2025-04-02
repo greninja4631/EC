fe20e3aa6ae9372484e82b5548a9f9a63895916e
joaoonvnjo <greninja4631@gmail.com>
2025-04-02 19:05:29 +0900

    git毎日練習

--- a.js
+++ a.js
@@ -1,21 +1,15 @@
-const num = 1;
-while (num < 5) {
+const count = 1;
+while (count < 5) {
     console.log(num)
-    num += 1;
+    num ++;
 }
 
 
 
 
 
-
-
-
-// let num = 3;
-
-// while (num < 100) {
-//   console.log(num);
-//   num = num * 3;
-// }
-
-// console.log("end");
+// //サンプルソースコード1
+// var count = 0;//変数countを0で初期化
+// while(count<=10){　//条件式　変数が10以下であることを確認
+//     console.log(count);　//変数countの中身を出力
+//     count++;//
