a372f3981fc4b608470cc8a1fc0a41b2aa9a0533
joaoonvnjo <greninja4631@gmail.com>
2025-04-30 16:39:57 +0900

    popスタックの勉強を行う

--- algo/mode_value.c
+++ algo/mode_value.c
@@ -7,3 +7,26 @@ typedef struct Node {
 } Node;
 
 
+
+
+
+
+
+
+
+
+
+int main() {
+    Node *head = NULL;
+    head = push(head, 10);
+    head = push(head, 20);
+    head = push(head, 30);
+
+    print_list(head);
+
+    head = delete_node(head, 20);
+    print_list(head);
+    
+    free_all(head);
+    return 0;
+}
