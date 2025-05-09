7f36d6535a94eaba4a42f0475938f5c7b646a5b3
joaoonvnjo <greninja4631@gmail.com>
2025-05-09 10:05:41 +0900

    Your message here

--- algo/single.c
+++ algo/single.c
@@ -6,45 +6,45 @@ typedef struct Node {
     struct Node *next;
 } Node;
 
-// ノードをリストの先頭に追加
-Node* push_front(Node *head, int val) {
+// 要素をリストの先頭に追加
+Node* push_front(Node *head, int value) {
     Node *new_node = malloc(sizeof(Node));
     if (!new_node) return head;
-    new_node->data = val;
+    new_node->data = value;
     new_node->next = head;
     return new_node;
 }
 
-// 値が key のノードを削除（ダミーノード方式ですべて cur->next で統一）
+// 指定された値を持つノードを削除
 Node* delete_node(Node *head, int key) {
-    if (!head) return NULL;
+    Node dummy = {0, head};
+    Node *prev = &dummy;
 
-    Node dummy = {0, head};    // data は 0（使わない）、next に本当の先頭をつなぐ
-    Node *cur = &dummy;      // これで cur->next が常に本物の head になる
+    while (prev->next && prev->next->data != key) 
+        prev = prev->next;
 
-    while (cur->next && cur->next->data != key)
-        cur = cur->next;
-
-    if (cur->next) {
-        Node *to_delete = cur->next;
-        cur->next = to_delete->next;
+    if (prev->next) { 
+        Node *to_delete = prev->next;
+        prev->next = to_delete->next;
         free(to_delete);
+    } else {
+        printf("Key %d not found in list\n", key);
     }
 
-    return dummy.next;
+    return dummy.next; 
 }
 
-// リスト全体を出力
+// リストの内容を表示
 void print_list(Node *head) {
-    while (head) {
-        printf("%d -> ", head->data);
-        head = head->next;
+    for (Node *cur = head; cur; cur = cur->next) {
+        printf("%d", cur->data);
+        if (cur->next) printf(" -> ");
     }
-    puts("NULL");
+    printf(" -> NULL\n");
 }
 
-// 全ノードを解放
-void free_all(Node *head) {
+// 全ノードのメモリを解放
+void free_list(Node *head) {
     while (head) {
         Node *next = head->next;
         free(head);
@@ -52,17 +52,17 @@ void free_all(Node *head) {
     }
 }
 
+// メイン処理
 int main() {
     Node *head = NULL;
     head = push_front(head, 10);
     head = push_front(head, 20);
-    head = push_front(head, 30);  // 30 -> 20 -> 10
+    head = push_front(head, 30);
 
-    print_list(head);             // 出力：30 -> 20 -> 10 -> NULL
+    print_list(head);                  // 30 -> 20 -> 10 -> NULL
+    head = delete_node(head, 20);     // 中間ノードを削除
+    print_list(head);                  // 30 -> 10 -> NULL
 
-    head = delete_node(head, 20); // 中間ノード削除（ダミーノード方式）
-    print_list(head);             // 出力：30 -> 10 -> NULL
-
-    free_all(head);
+    free_list(head);
     return 0;
 }
--- algo/single_practice.c
+++ algo/single_practice.c
@@ -3,80 +3,57 @@
 
 // ノードの構造定義
 typedef struct Node {
-  int data;
-  struct Node *next;
+    int data;
+    struct Node *next;
 } Node;
 
 // ノードを先頭に追加する関数
-Node *push(Node *head, int val) {
-  Node *tmp = malloc(sizeof(Node));
-  if (!tmp) {
-    perror("malloc failed");
-    exit(1);
-  }
-  tmp->data = val;
-  tmp->next = head;
-  return tmp;
+Node* push(Node *head , int val) {
+Node *tmp = malloc(sizeof(Node));
+if(!tmp) return NULL;
+
+head->data = val;
+head->next = head;
+return tmp;
 }
 
 // リストから指定値を削除する関数（最初に見つかった1つだけ）
-Node *delete(Node *head, int key) {
-  if (!head)
-    return NULL;
+Node* delete(Node *head , int key) {
+Node dummy = {0 , head};
+Node *cur = &dummy;
 
-  Node dummy = {0, head}; // ダミーヘッドを使う
-  Node *cur = &dummy;
+if(*cur->next && *cur->next->data !=key) {
 
-  while (cur->next && cur->next->data != key) {
-    cur = cur->next;
-  }
-
-  if (cur->next) {
-    Node *tmp = cur->next;
-    cur->next = tmp->next;
-    free(tmp);
-  }
-
-  return dummy.next;
 }
 
+
+
+}
 // リストを表示する関数
-void list(Node *head) {
-  for (; head; head = head->next) {
-    printf("%d -> ", head->data);
-  }
-  printf("NULL\n");
-}
+
 
 // リストをすべて解放する関数
-void free_list(Node *head) {
-  while (head) {
-    Node *tmp = head;
-    head = head->next;
-    free(tmp);
-  }
-}
+
 
 int main() {
-  Node *head = NULL;
+    Node *head = NULL;
 
-  // 要素追加
-  head = push(head, 10);
-  head = push(head, 20);
-  head = push(head, 30);
+    // 要素追加
+    head = push(head, 10);
+    head = push(head, 20);
+    head = push(head, 30);
 
-  // 表示
-  printf("Initial list:\n");
-  list(head);
+    // 表示
+    printf("Initial list:\n");
+    list(head);
 
-  // 要素削除
-  head = delete (head, 20);
-  printf("After deleting 20:\n");
-  list(head);
+    // 要素削除
+    head = delete(head, 20);
+    printf("After deleting 20:\n");
+    list(head);
 
-  // メモリ解放
-  free_list(head);
+    // メモリ解放
+    free_list(head);
 
-  return 0;
+    return 0;
 }
-  
